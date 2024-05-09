452d8903ada90e128be4bfad7586c0c674ca7eb8
Mehtab <150262610+Mehtab-786@users.noreply.github.com>
2024-05-09 10:12:19 +0000

    main code

--- colorChanger/app.js
+++ colorChanger/app.js
@@ -1,1 +1,27 @@
-console.log("Mehtab");
+let btns = document.querySelectorAll(".button")
+let body = document.querySelector("body")
+
+btns.forEach(function (button) {
+    
+    button.addEventListener("click", function (e) {
+        
+        let color = e.target.id;
+        body.style.backgroundColor = color;
+
+
+// ---------other way----------
+        // if (color === "red"){
+        //     body.style.backgroundColor = color;
+        // } else if (color === "green") {
+        //     body.style.backgroundColor = color;
+        // } else if (color === "silver") {
+        //     body.style.backgroundColor = color;
+        // } else if (color === "blue") {
+        //     body.style.backgroundColor = color;
+        // } else if (color === "black") {
+        //     body.style.backgroundColor = color;
+        // } else {
+        //     body.style.backgroundColor  = color;
+        // }
+        })
+})
